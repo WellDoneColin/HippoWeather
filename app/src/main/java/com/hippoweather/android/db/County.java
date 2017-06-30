92b87e923e9a8ab31cd65d286f1de19cf86d829f
ColinZhang <vanroan46@163.com>
2017-06-30 14:05:53 +0800

    配置数据库和表

---
+++ app/src/main/java/com/hippoweather/android/db/County.java
@@ -0,0 +1,50 @@
+package com.hippoweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by colin on 2017/6/30.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+
+    private String countryName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
